29bd057c16990aee9f8726c5a4925d260dc4b291
RSaucedo96 <ricardoarielsaucedo96@gmail.com>
2022-10-12 17:33:47 -0300

    implementing module pattern.

--- tateti.js
+++ tateti.js
@@ -9,6 +9,15 @@ game ends
 */
 
 
+const playerOne = new Player ();
+const playerTwo = new Player ();
+
+const playerOneForm = document.getElementById("p1"); 
+playerOneForm.addEventListener("submit", playerOne.log);
+
+const playerTwoForm = document.getElementById("p2"); 
+playerTwoForm.addEventListener("submit", playerTwo.log );
+
 const documentMock = (() => ({
     querySelector: (selector) => ({
       innerHTML: null,
@@ -19,17 +28,19 @@ const Board = (function() {
     var board=[[],[],[]];
     
     const _boardState = (message) => {
+      
     };
 
     const _resetBoard = () => {
+      //clean board array
       var j;
       for (var i = board[j].length; i > 0; i--) {
         board[j].pop();
         j ++;
        }
-       j=0;
+      j=0;
        
-       /* clean interface */
+      // clean web board
       
     };
   
@@ -50,21 +61,24 @@ const Board = (function() {
     }
 })(document || documentMock);
 
-const Player = (function(doc) {
-    const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
+const Player = (function() {
+
+  const log = (e) => {
+    e.preventDefault();
+    const playerName = document.querySelector('#p1').value;
+  };
   
-    const makeUppercase = (text) => {
+  const makeUppercase = (text) => {
       log("Making uppercase");
       return text.toUpperCase();
-    };
+  };
   
-    const writeToDOM = (selector, message) => {
-      doc.querySelector(selector).innerHTML = message;
-    }
+  const writeToDOM = (selector, message) => {
+    doc.querySelector(selector).innerHTML = message;
+  }
   
     return {
-      makeUppercase,
-      writeToDOM,
+      log
     }
 })(document || documentMock);
 
@@ -73,27 +87,27 @@ const Game = (function(doc) {
 
 
     const _score = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
-  
-    const _turn = (text) => {
+      
+
+    const _turn = () => {
       log("Making uppercase");
       return text.toUpperCase();
     };
-    
-    const logPlayers = (selector, message) => {
-      doc.querySelector(selector).innerHTML = message;
-    }
 
-    const playRound = (selector, message) => {
+    const _playRound = (selector, message) => {
       doc.querySelector(selector).innerHTML = message;
-    }
-    
+    };
+
+    const playMatch = () => {
+
+    };
+
     const _winCondition = (selector, message) => {
       doc.querySelector(selector).innerHTML = message;
-    }
+    };
   
     return {
-      makeUppercase,
-      writeToDOM,
+        
     }
 })(document || documentMock);
   
