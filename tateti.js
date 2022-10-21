72ca3c934e0d7a303929bbb374801bdbef7928cb
Ricardo <ricardoarielsaucedo96@gmail.com>
2022-10-21 09:59:56 -0300

    images for players created

--- tateti.js
+++ tateti.js
@@ -38,10 +38,10 @@ const Board = (function() {
       for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
           if (board[i][j] === 1){
-            document.getElementById(`${i+1}${j+1}`).style.backgroundImage = "url('./img/basictile.png')";
+            document.getElementById(`${i+1}${j+1}`).style.backgroundImage = "url('./img/circle.png')";
           }
           else if (board[i][j] === 2){
-            document.getElementById(`${i+1}${j+1}`).style.backgroundImage = "url('./img/basictile.png')";
+            document.getElementById(`${i+1}${j+1}`).style.backgroundImage = "url('./img/cross.png')";
           }
           else if (board[i][j] === 0){
             document.getElementById(`${i+1}${j+1}`).style.backgroundImage = "url('./img/basictile.png')";
